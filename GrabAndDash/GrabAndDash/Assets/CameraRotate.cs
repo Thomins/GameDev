ad1460cd3a5aa449866ce41971e6c1471fdeec7e
SteeledSlagle13 <SteeledSlagle13@gmail.com>
2018-09-17 00:57:38 -0500

    Camera View
    3 Cameras added
    2 Walls added
    This is for testing camera View
    next step adding power ups

---
+++ GrabAndDash/GrabAndDash/Assets/CameraRotate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour {
+
+    public float RoatationSpeed = 0.1f;
+    bool right = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.rotation.eulerAngles.y <= 120 || transform.rotation.eulerAngles.y >= 160)
+        {
+            RoatationSpeed *= -1;
+        }
+        transform.eulerAngles = new Vector3(10, transform.rotation.eulerAngles.y + RoatationSpeed, 0);
+    }
+}
