2b99043e7969f1132c79a66840ae74adffdaa53e
David <dpeek2010@yahoo.com>
2018-10-17 18:42:30 -0500

    menu/timer/powerups

---
+++ GameDev-master/GrabAndDash/Assets/Scripts/acquireDonut.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class acquireDonut : MonoBehaviour {
+
+    public GameObject player;
+    public float distance;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameObject.transform.position.x - player.transform.position.x <= distance &&
+            gameObject.transform.position.z - player.transform.position.z <= distance)
+        {
+            //delete object
+            Destroy(gameObject);
+        }
+	}
+}
