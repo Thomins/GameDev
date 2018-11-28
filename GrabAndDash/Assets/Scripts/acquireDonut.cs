908d36aeb40f7e92328cddbf35f507737e98f84c
Jojo <SteeledSlagle13@gmail.com>
2018-11-28 09:53:42 -0600

    AssetsFolder Added

---
+++ GrabAndDash/Assets/Scripts/acquireDonut.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class acquireDonut : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision with " + collision.gameObject.name);
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player has come in range of Doughnut.");
+        }
+    }
+
+}
